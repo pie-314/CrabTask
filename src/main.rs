f9a0fce36d88852a94617014a274ee6b0d819927
pie-314 <aadarsh.official.xz@gmail.com>
2025-08-08 12:26:42 +0530

    Basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use color_eyre::{
-    eyre::{Ok, Result},
-    owo_colors::OwoColorize,
-};
+use color_eyre::eyre::{Ok, Result};
 use ratatui::{
     crossterm::event::{self, Event},
     layout::{Constraint, Layout},
@@ -10,18 +7,24 @@ use ratatui::{
     widgets::{block::title, Block, BorderType, List, Paragraph, Widget},
     DefaultTerminal, Frame,
 };
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use std::result::Result::Err;
+use std::result::Result::Ok as Okk;
 
 #[derive(Debug, Default)]
 struct AppState {
-    items: Vec<TodoItem>,
+    tasks: Vec<Todo>,
 }
 
-#[derive(Debug, Default)]
-struct TodoItem {
+#[derive(Deserialize, Serialize, Debug)]
+struct Todo {
+    date: String,
+    task: String,
     is_done: bool,
-    description: String,
 }
 
+//Main function
 fn main() -> Result<()> {
     let mut state = AppState::default();
     color_eyre::install()?;
@@ -31,6 +34,28 @@ fn main() -> Result<()> {
     result
 }
 
+// JSON parsing
+fn json_parser(date: &str) -> Option<Todo> {
+    let json = std::fs::read_to_string("data/todo_data.json").unwrap();
+    let todos = from_str::<Vec<Todo>>(&json);
+
+    match todos {
+        Okk(value) => {
+            for daily in value {
+                if daily.date == date {
+                    return Some(daily);
+                }
+            }
+            None
+        }
+        Err(erro) => {
+            println!("Error parsing JSON: {:#?}", erro);
+            None
+        }
+    }
+}
+
+// Loading UI
 fn ui(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
@@ -49,6 +74,7 @@ fn ui(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     Ok(())
 }
 
+// Rendering the UI
 fn render(frame: &mut Frame, app_state: &AppState) {
     let [border_area] = Layout::vertical([Constraint::Fill(1)])
         .margin(1)
@@ -57,10 +83,46 @@ fn render(frame: &mut Frame, app_state: &AppState) {
     let title = Line::from("CrabTask".bold());
     Block::bordered()
         .title(title.centered())
-        .border_type(BorderType::Thick)
-        .fg(Color::Yellow)
+        .border_type(BorderType::Rounded)
+        .fg(Color::Magenta)
         .render(border_area, frame.buffer_mut());
-    //List::new(app_state::items).render(area, buf);
 
-    //Paragraph::new("Hello from the application").render(frame.area(), frame.buffer_mut());
+    let [full_top, full_bottom] =
+        Layout::vertical([Constraint::Percentage(90), Constraint::Percentage(10)])
+            .margin(0)
+            .areas(border_area);
+
+    let [left_area, right_area] =
+        Layout::horizontal([Constraint::Percentage(70), Constraint::Percentage(30)])
+            .margin(1)
+            .areas(full_top);
+
+    let [right_top, right_middle, right_bottom] = Layout::vertical([
+        Constraint::Percentage(40),
+        Constraint::Percentage(20),
+        Constraint::Percentage(40),
+    ])
+    .margin(0)
+    .areas(right_area);
+
+    //stats progress bar
+    let status = Line::from("PROGRESS".bold().italic());
+    Block::bordered()
+        .title(status)
+        .border_type(BorderType::Rounded)
+        .render(right_top, frame.buffer_mut());
+
+    // All the necessary key bindings
+    let keywords = Line::from("calender".bold().italic());
+    Block::bordered()
+        .title(keywords)
+        .border_type(BorderType::Rounded)
+        .render(right_bottom, frame.buffer_mut());
+
+    //Each day Task
+    let today = Line::from("Today".bold().italic());
+    Block::bordered()
+        .title(today)
+        .border_type(BorderType::Rounded)
+        .render(left_area, frame.buffer_mut());
 }
